fadd0f424a52f154ec012d3a9aa2c03d31a0f800
Tomasz Idziak <tomasz.idziak@gmail.com>
2016-06-28 20:44:32 +0200

    Initial maven project

---
+++ src/main/java/edu/agh/idziak/astarw/Main.java
@@ -0,0 +1,7 @@
+package edu.agh.idziak.astarw;
+
+/**
+ * Created by Tomasz on 28.06.2016.
+ */
+public class Main {
+}
---
+++ src/test/java/edu/agh/idziak/astarw/MainTest.java
@@ -0,0 +1,15 @@
+package edu.agh.idziak.astarw;
+
+import org.junit.Test;
+
+/**
+ * Created by Tomasz on 28.06.2016.
+ */
+public class MainTest {
+
+    @Test
+    public void test1() throws Exception {
+        
+
+    }
+}
